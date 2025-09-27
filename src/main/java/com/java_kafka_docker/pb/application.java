df0c41e0202ad665af0e821fed96c64fcc83d762
pradeep <pradeepb3093@gmail.com>
2025-09-27 16:20:00 +0100

    dockerized application

--- src/main/java/com/java_kafka_docker/pb/application.java
+++ src/main/java/com/java_kafka_docker/pb/application.java
@@ -2,8 +2,6 @@ package com.java_kafka_docker.pb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class application {
