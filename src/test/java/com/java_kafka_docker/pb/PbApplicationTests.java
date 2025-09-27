948419b08e5707d2ae9360ff2adb48cd684054d0
pradeep balasubramaniam <pradeepb3093@gmail.com>
2025-09-27 15:03:46 +0100

    inital commit with just springboot starter

---
+++ src/test/java/com/java_kafka_docker/pb/PbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_kafka_docker.pb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
